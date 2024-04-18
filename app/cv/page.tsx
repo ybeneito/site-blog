205d46d9c63e18d91830eaaf292a3e1286d6e84f
Yohan Beneito <y.beneito@gmail.com>
2024-04-18 06:45:56 +0200

    fix cv & link

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -2,7 +2,7 @@ export default function page() {
     return (
         <div className="min-h-screen">
             <h1>Mon CV</h1>
-            <embed src="/pdfs/CV2024.pdf" width="100%" height="1024px" type="application/pdf" />
+            <embed src="/pdfs/cv.pdf" width="100%" height="1024px" type="application/pdf" />
         </div>
     )
 }
